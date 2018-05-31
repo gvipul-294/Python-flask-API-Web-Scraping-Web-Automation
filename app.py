3f957a893045fd5804b0c7a8c3891af925ed7d46
Vipul Gupta <gvipul294@gmail.com>
2018-05-31 09:39:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,58 +1,50 @@
-from flask import Flask, request, jsonify
-from pymongo import MongoClient
-from bson import json_util
-
-MONGODB_URI = "mongodb://test:test@ds227459.mlab.com:27459/student_record"
-client = MongoClient(MONGODB_URI)
-db = client.get_database("student_record")
-student_record = db.student_records
-
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+import csv
 
 
-@app.route('/')
-def index():
-	return "Welcome to student records API!"
+app= Flask("my flask app!")
+
+@app.route('/home')
+def home():
+	return render_template("index.html")
+
+@app.route('/blog/<id>/')
+def blog(id):
+	return "<h1>Showing blog no. </h1>" + id
+
+@app.route('/bio')
+def mybio():
+	name = request.args.get('name')
+	age = request.args.get('age')
+	names=['a', 'b', 'c']
+	return render_template("index.html", name=name, age=age, names=names)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_data():
+	if request.method=='GET':
+		return render_template('upload.html')
+	else:
+		name = request.form.get('name')
+		age = request.form.get('age')
+		branch = request.form.get('branch')
+
+		with open("student_db.csv", "a") as f:
+			writer= csv.writer(f)
+			writer.writerow([name, age, branch])
+
+		"""Alternative
+		with open("student_db.csv", "a") as f:
+			writer= csv.Dictwriter(f, fieldnames=["name", "age", "branch"]) 	_check
+			writer.writerow(request.form)
+		"""
+
+		image = request.files.get('image')
+		image.save('static/images/'+image.filename)
+		return redirect(url_for('home'))
 
 
-@app.route('/students/', methods=['GET', 'POST'])
-def student_list():
-	if request.method == 'GET':
-		students = list(student_record.find())
-		return json_util.dumps(students), 200
-
-	elif request.method == 'POST':
-		roll_no = int(request.form.get("roll_no"))
-
-		if student_record.find_one({"roll_no":roll_no}):
-			return jsonify({"error": "User already registered!"}), 400
-
-		student = {}
-		student['roll_no'] = roll_no
-		student['name'] = request.form.get("name")
-		student['age'] = request.form.get("age")
-		student['branch'] = request.form.get("branch")
-
-		student_record.insert_one(student)
-
-		return jsonify({"result": "Student successfully registered!"}), 201
+if __name__=="__main__":
+	app.run(host="0.0.0.0", port=7000, use_reloader=True)
 
 
-@app.route('/student/<int:roll_no>/', methods=['GET', 'PATCH', 'DELETE'])
-def get_student(roll_no):
-	student = student_record.find_one({"roll_no":roll_no})
-	if not student:
-		return jsonify({"error": "Student is not registered."}), 404
-
-	if request.method == 'GET':
-		return json_util.dumps(student), 200
-	elif request.method == 'PATCH':
-		student_record.update_one({'roll_no':roll_no}, {'$set':request.form})
-		return jsonify({"result": "Student record successfully updated!"})
-	elif request.method == 'DELETE':
-		student_record.delete_one({'roll_no':roll_no})
-		return jsonify({"result": "Student record successfully deleted!"}), 204
-
-
-if __name__ == "__main__":
-	app.run(port=8001, use_reloader=True, debug=True)
+	"""myenv\Scripts\activate"""
